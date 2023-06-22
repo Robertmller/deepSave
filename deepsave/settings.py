345c5fd963729f98386fd035e61191e80a856bcf
Robert Muller <contatorobertmull@gmail.com>
2023-06-22 18:48:47 -0300

    fix: removed duplicated rule

--- deepsave/settings.py
+++ deepsave/settings.py
@@ -58,12 +58,6 @@ TEMPLATES = [
     },
 ]
 
-# Base url to serve media files
-MEDIA_URL = '/media/'
-
-# Path where media is stored
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-
 WSGI_APPLICATION = 'deepsave.wsgi.application'
 
 DATABASES = {
